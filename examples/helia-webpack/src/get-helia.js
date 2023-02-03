28f7b87ea90162021b47291d862644c9026447a6
Alex Potsides <alex@achingbrain.net>
2023-02-03 13:29:47 +0100

    feat: add example for bundling with webpack (#2)
    Ports js-ipfs example to helia

---
+++ examples/helia-webpack/src/get-helia.js
@@ -0,0 +1,47 @@
+import { createHelia } from 'helia'
+import { createLibp2p } from 'libp2p'
+import { noise } from '@chainsafe/libp2p-noise'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { webSockets } from '@libp2p/websockets'
+import { bootstrap } from '@libp2p/bootstrap'
+import { MemoryBlockstore } from 'blockstore-core'
+import { MemoryDatastore } from 'datastore-core'
+
+export async function getHelia () {
+  // the blockstore is where we store the blocks that make up files
+  const blockstore = new MemoryBlockstore()
+
+  // application-specific data lives in the datastore
+  const datastore = new MemoryDatastore()
+
+  // libp2p is the networking layer that underpins Helia
+  const libp2p = await createLibp2p({
+    datastore,
+    transports: [
+      webSockets()
+    ],
+    connectionEncryption: [
+      noise()
+    ],
+    streamMuxers: [
+      yamux()
+    ],
+    peerDiscovery: [
+      bootstrap({
+        list: [
+          "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+          "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+          "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+          "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt"
+        ]
+      })
+    ]
+  })
+
+  // create a Helia node
+  return await createHelia({
+    datastore,
+    blockstore,
+    libp2p
+  })
+}
