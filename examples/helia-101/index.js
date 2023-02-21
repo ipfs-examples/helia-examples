c544415eb2074acdf78360bea8ad9d1c3248d771
Daniel N <2color@users.noreply.github.com>
2023-02-21 14:06:12 +0100

    connect to node with webtransport

--- examples/helia-101/index.js
+++ examples/helia-101/index.js
@@ -3,11 +3,13 @@ import { createLibp2p } from 'libp2p'
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
 import { webSockets } from '@libp2p/websockets'
+import { webTransport } from '@libp2p/webtransport'
 import { bootstrap } from '@libp2p/bootstrap'
 import { unixfs } from '@helia/unixfs'
 import { MemoryBlockstore } from 'blockstore-core'
 import { MemoryDatastore } from 'datastore-core'
 import { importBytes } from 'ipfs-unixfs-importer'
+import { peerIdFromString } from '@libp2p/peer-id'
 
 // the blockstore is where we store the blocks that make up files
 const blockstore = new MemoryBlockstore()
@@ -18,32 +20,31 @@ const datastore = new MemoryDatastore()
 // libp2p is the networking layer that underpins Helia
 const libp2p = await createLibp2p({
   datastore,
-  transports: [
-    webSockets()
-  ],
-  connectionEncryption: [
-    noise()
-  ],
-  streamMuxers: [
-    yamux()
-  ],
+  transports: [webSockets(), webTransport()],
+  connectionEncryption: [noise()],
+  streamMuxers: [yamux()],
   peerDiscovery: [
     bootstrap({
       list: [
-        "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
-        "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
-        "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
-        "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt"
-      ]
-    })
-  ]
+        '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+        '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+        '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+        '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
+      ],
+    }),
+  ],
 })
+
+const connection = await libp2p.dial(
+  peerIdFromString('12D3KooW9snnuzHgfzpBKWtZxU9tpPDqB7SG4qM9tGLA9eQgYpQh'),
+)
+console.log(connection)
 
 // create a Helia node
 const helia = await createHelia({
   datastore,
   blockstore,
-  libp2p
+  libp2p,
 })
 
 // print out our node's PeerId
@@ -57,7 +58,10 @@ const fs = unixfs(helia)
 const encoder = new TextEncoder()
 
 // add the bytes to your node and receive a unique content identifer
-const { cid } = await importBytes(encoder.encode('Hello World 101'), helia.blockstore)
+const { cid } = await importBytes(
+  encoder.encode('Hello World 101'),
+  helia.blockstore,
+)
 
 console.log('Added file:', cid.toString())
 
@@ -67,7 +71,7 @@ let text = ''
 
 for await (const chunk of fs.cat(cid)) {
   text += decoder.decode(chunk, {
-    stream: true
+    stream: true,
   })
 }
 
