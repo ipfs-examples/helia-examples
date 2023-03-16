7e5116a2d9b54d0c84c4e07d17fe34fd7bc62cd8
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-03-16 15:32:43 -0700

    feat(helia-script-tag): fluff up example

--- examples/helia-script-tag/src/index.js
+++ examples/helia-script-tag/src/index.js
@@ -2,22 +2,82 @@
 
 // not imported from skypack nor unpkg because of issues.
 import { noise } from 'https://esm.sh/v111/@chainsafe/libp2p-noise@11.0.1/es2022/libp2p-noise.js'
-// console.log(`noise: `, noise);
 
-let libp2pInstance = null
-// const createLibp2p = async ({ datastore }) => {
+const statusEl = document.getElementById('status')
+const statusValueEl = document.getElementById('statusValue')
+const discoveredPeerCount = document.getElementById('discoveredPeerCount')
+const connectedPeerCount = document.getElementById('connectedPeerCount')
+const connectedPeersList = document.getElementById('connectedPeersList')
 
-//   if (libp2pInstance != null) {
-//     return libp2pInstance
-//   }
-//   // console.log(window.Libp2P)
-//   // libp2p is the networking layer that underpins Helia
+document.addEventListener('DOMContentLoaded', async () => {
+  window.helia = await createHelia()
+  window.heliaFs = await HeliaUnixfs.unixfs(helia)
+
+  helia.libp2p.addEventListener('peer:discovery', (evt) => {
+    discoveredPeers.set(evt.detail.id.toString(), evt.detail)
+    addToLog(`Discovered peer ${evt.detail.id.toString()}`)
+  })
+
+  helia.libp2p.addEventListener('peer:connect', (evt) => {
+    addToLog(`Connected to ${evt.detail.remotePeer.toString()}`)
+  })
+  helia.libp2p.addEventListener('peer:disconnect', (evt) => {
+    addToLog(`Disconnected from ${evt.detail.remotePeer.toString()}`)
+  })
+
+  setInterval(() => {
+    statusValueEl.innerHTML = window.helia.libp2p.started ? 'Online' : 'Offline'
+    updateConnectedPeers()
+    updateDiscoveredPeers()
+  }, 500)
+
+  const id = await helia.libp2p.peerId.toString()
+
+  const nodeIdEl = document.getElementById("nodeId");
+  nodeIdEl.innerHTML = id
+
+  /**
+   * You can write more code here to use it.
+   *
+   * https://github.com/ipfs/helia
+   * - helia.start
+   * - helia.stop
+   *
+   * https://github.com/ipfs/helia-unixfs
+   * - heliaFs.addBytes
+   * - heliaFs.addFile
+   * - heliaFs.ls
+   * - heliaFs.cat
+   */
+})
 
 
-//   return libp2pInstance
-// }
+function ms2TimeString(a){
+  const k=a%1e3
+  const s=a/1e3%60|0
+  const m=a/6e4%60|0
+  const h=a/36e5%24|0
+
+  return (h ? (h<10?'0'+h:h)+':':'00:')+
+    (m<10?0:'')+m+':'+
+    (s<10?0:'')+s+':'+
+    (k<100?k<10?'00':0:'')+k
+}
+
+const logEl = document.getElementById('runningLog')
+
+const getLogLineEl = (msg) => {
+  const logLine = document.createElement('span')
+  logLine.innerHTML = `${ms2TimeString(performance.now())} - ${msg}`
+
+  return logLine
+}
+const addToLog = (msg) => {
+  logEl.appendChild(getLogLineEl(msg))
+}
 
 let heliaInstance = null
+let libp2pInstance = null
 const createHelia = async () => {
 
   // application-specific data lives in the datastore
@@ -29,16 +89,19 @@ const createHelia = async () => {
   }
 
   if (libp2pInstance == null) {
+
     libp2pInstance = await Libp2P.createLibp2p({
       datastore,
+      dht: Libp2PKadDht.kadDHT(),
       transports: [
-        Libp2PWebsockets.webSockets()
+        Libp2PWebsockets.webSockets(),
       ],
       connectionEncryption: [
         noise()
       ],
       streamMuxers: [
-        ChainsafeLibp2PYamux.yamux()
+        ChainsafeLibp2PYamux.yamux(),
+        Libp2PMplex.mplex(),
       ],
       peerDiscovery: [
         Libp2PBootstrap.bootstrap({
@@ -48,10 +111,36 @@ const createHelia = async () => {
             "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
             "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt"
           ],
-          timeout: 60000
-        })
-      ]
+        }),
+      ],
+      dialer: {
+        dialTimeout: 120000,
+      },
+      peerStore: {
+        persistence: true,
+        /**
+         * The threshold number represents the maximum number of "dirty peers" allowed in the PeerStore, i.e. peers that
+         * are not updated in the datastore. In this context, browser nodes should use a threshold of 1, since they
+         * might not "stop" properly in several scenarios and the PeerStore might end up with unflushed records when the
+         * window is closed.
+         * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#configuring-peerstore
+         */
+        threshold: 1
+      },
+      config: {
+        dht: {                        // The DHT options (and defaults) can be found in its documentation
+          kBucketSize: 5,
+          enabled: true,
+          randomWalk: {
+            enabled: true,            // Allows to disable discovery (enabled by default)
+            interval: 300e3,
+            timeout: 10e3
+          }
+        }
+      }
     })
+    addToLog('Created LibP2P instance')
+
   }
 
   heliaInstance = await Helia.createHelia({
@@ -59,64 +148,25 @@ const createHelia = async () => {
     blockstore,
     libp2p: libp2pInstance
   })
+  addToLog('Created Helia instance')
 
   return heliaInstance
 }
 
-const testFn = async () => {
-  console.log('testFn triggered')
-  await new Promise((resolve, reject) => {
-    console.log('testFn promise triggered')
-    setTimeout(() => {
-      console.log('timeout callback triggered')
-      resolve()
-    }, 5000)
-  })
-  console.log('testFn promise resolved')
+window.discoveredPeers = new Map()
+
+const updateConnectedPeers = () => {
+  const peers = helia.libp2p.getPeers()
+  connectedPeerCount.innerHTML = peers.length
+  connectedPeersList.innerHTML = ''
+  for (const peer of peers) {
+    const peerEl = document.createElement('li')
+    peerEl.innerText = peer.toString()
+    connectedPeersList.appendChild(peerEl)
+  }
 }
-document.addEventListener('DOMContentLoaded', async () => {
-  console.log(`DOMContentLoaded: `);
-  await testFn()
-  console.log(`Helia: `, Helia);
-  console.log(`Libp2P: `, Libp2P);
-  console.log(`ChainsafeLibp2PYamux: `, ChainsafeLibp2PYamux);
-  console.log(`Libp2PWebsockets: `, Libp2PWebsockets);
-  console.log(`Libp2PBootstrap: `, Libp2PBootstrap);
-  console.log(`DatastoreCore: `, DatastoreCore);
-  console.log(`BlockstoreCore: `, BlockstoreCore);
-  console.log('after await testFn')
-  await createHelia()
-  console.log('heliaInstance', heliaInstance)
-  heliaInstance.libp2p.connectionManager.onConnect((event) => {
-    console.log('event', event)
-  })
-  // heliaInstance.libp2p.connectionManager.onDisconnect((event) => {
-  //   console.log('event', event)
-  // })
-  // console.log()
-  // const insertAfter = (referenceNode, newNode) => {
-  //   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-  // }
 
-  // const node = await Ipfs.create({ repo: 'ipfs-' + Math.random() })
-  // window.node = node
+const updateDiscoveredPeers = () => {
+  discoveredPeerCount.innerHTML = discoveredPeers.size
+}
 
-  // const status = node.isOnline() ? 'online' : 'offline'
-  // const id = await node.id()
-
-  // console.log(`Node status: ${status}`)
-
-  // const statusDOM = document.getElementById('status')
-  // statusDOM.innerHTML = `Node status: ${status}`
-
-  // const newDiv = document.createElement("div");
-  // newDiv.id = "node"
-  // const newContent = document.createTextNode(`ID: ${id.id}`);
-  // newDiv.appendChild(newContent);
-
-  // insertAfter(statusDOM, newDiv);
-
-  // You can write more code here to use it. Use methods like
-  // node.add, node.get. See the API docs here:
-  // https://github.com/ipfs/js-ipfs/tree/master/packages/interface-ipfs-core
-})
