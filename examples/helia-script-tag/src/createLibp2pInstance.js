6fda9d9da764ff8d2f403fd75e277719f58d8ca4
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-03-22 11:16:10 -0700

    chore: address PR comments

---
+++ examples/helia-script-tag/src/createLibp2pInstance.js
@@ -0,0 +1,89 @@
+/** global Helia, Libp2P, ChainsafeLibp2PYamux, Libp2PWebsockets, Libp2PBootstrap, BlockstoreCore, DatastoreCore, Libp2PKadDht, Libp2PMplex */
+// not imported from skypack,jsdelivr, nor unpkg because of issues. see https://github.com/ChainSafe/js-libp2p-noise/issues/296
+import { noise } from 'https://esm.sh/v111/@chainsafe/libp2p-noise@11.0.1/es2022/libp2p-noise.js'
+
+/**
+ *
+ * @param {object} param0
+ * @param {import('libp2p').Libp2pOptions['datastore']} param0.datastore
+ */
+export async function createLibp2pInstance ({datastore}) {
+  /** @type {Partial<import('libp2p').Libp2pOptions>} */
+  const libp2pInit = {
+    /**
+     * @see https://github.com/ipfs/js-ipfs-interfaces/tree/master/packages/interface-datastore
+     */
+    datastore,
+    /**
+     * @see https://github.com/libp2p/js-libp2p-kad-dht
+     * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#customizing-dht
+     */
+    dht: /** @type {import('libp2p').Libp2pOptions['dht']} */(Libp2PKadDht.kadDHT({                        // The DHT options (and defaults) can be found in its documentation
+      kBucketSize: 5,
+      clientMode: true,
+      enabled: true,
+      randomWalk: {
+        enabled: true,            // Allows to disable discovery (enabled by default)
+        interval: 300e3,
+        timeout: 10e3
+      }
+    })),
+    /**
+     * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#transport
+     */
+    transports: /** @type {import('libp2p').Libp2pOptions['transports']} */([
+      Libp2PWebsockets.webSockets(),
+    ]),
+    /**
+     * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#connection-encryption
+     */
+    connectionEncryption: /** @type {import('libp2p').Libp2pOptions['connectionEncryption']} */([
+      noise()
+    ]),
+    /**
+     * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#stream-multiplexing
+     */
+    streamMuxers: /** @type {import('libp2p').Libp2pOptions['streamMuxers']} */([
+      ChainsafeLibp2PYamux.yamux(),
+      Libp2PMplex.mplex(),
+    ]),
+    /**
+     * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#customizing-peer-discovery
+     */
+    peerDiscovery: /** @type {import('libp2p').Libp2pOptions['peerDiscovery']} */([
+      Libp2PBootstrap.bootstrap({
+        list: [
+          "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+          "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+          "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+          "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt"
+        ],
+      }),
+    ]),
+    /**
+     * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#configuring-connection-manager
+     */
+    connectionManager: /** @type {import('libp2p').Libp2pOptions['connectionManager']} */({
+      // Auto connect to discovered peers (limited by ConnectionManager minConnections)
+      autoDial: true,
+    }),
+    /**
+     * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#configuring-peerstore
+     */
+    peerStore: /** @type {import('libp2p').Libp2pOptions['peerStore']} */({
+      persistence: true,
+      /**
+       * The threshold number represents the maximum number of "dirty peers" allowed in the PeerStore, i.e. peers that
+       * are not updated in the datastore. In this context, browser nodes should use a threshold of 1, since they
+       * might not "stop" properly in several scenarios and the PeerStore might end up with unflushed records when the
+       * window is closed.
+       */
+      threshold: 1
+    }),
+  };
+
+  /** @type {import('libp2p').Libp2p} */
+  const libp2pInstance = await Libp2P.createLibp2p(libp2pInit)
+
+  return libp2pInstance
+}
--- examples/helia-script-tag/src/index.js
+++ examples/helia-script-tag/src/index.js
@@ -1,17 +1,17 @@
 /** global Helia, Libp2P, ChainsafeLibp2PYamux, Libp2PWebsockets, Libp2PBootstrap, BlockstoreCore, DatastoreCore */
 
-// not imported from skypack,jsdelivr, nor unpkg because of issues.
-import { noise } from 'https://esm.sh/v111/@chainsafe/libp2p-noise@11.0.1/es2022/libp2p-noise.js'
+import { createLibp2pInstance } from './createLibp2pInstance.js'
 
-const statusEl = document.getElementById('status')
 const statusValueEl = document.getElementById('statusValue')
-const discoveredPeerCount = document.getElementById('discoveredPeerCount')
-const connectedPeerCount = document.getElementById('connectedPeerCount')
-const connectedPeersList = document.getElementById('connectedPeersList')
+const discoveredPeerCountEl = document.getElementById('discoveredPeerCount')
+const connectedPeerCountEl = document.getElementById('connectedPeerCount')
+const connectedPeersListEl = document.getElementById('connectedPeersList')
+const logEl = document.getElementById('runningLog')
+const nodeIdEl = document.getElementById('nodeId');
 
 let nodeUpdateInterval = null
 document.addEventListener('DOMContentLoaded', async () => {
-  window.helia = await createHelia()
+  window.helia = await instantiateHeliaNode()
   window.heliaFs = await HeliaUnixfs.unixfs(helia)
 
   helia.libp2p.addEventListener('peer:discovery', (evt) => {
@@ -27,14 +27,13 @@ document.addEventListener('DOMContentLoaded', async () => {
   })
 
   nodeUpdateInterval = setInterval(() => {
-    statusValueEl.innerHTML = window.helia.libp2p.started ? 'Online' : 'Offline'
+    statusValueEl.innerHTML = helia.libp2p.started ? 'Online' : 'Offline'
     updateConnectedPeers()
     updateDiscoveredPeers()
   }, 500)
 
   const id = await helia.libp2p.peerId.toString()
 
-  const nodeIdEl = document.getElementById("nodeId");
   nodeIdEl.innerHTML = id
 
   /**
@@ -65,8 +64,6 @@ function ms2TimeString(a){
     (k<100?k<10?'00':0:'')+k
 }
 
-const logEl = document.getElementById('runningLog')
-
 const getLogLineEl = (msg) => {
   const logLine = document.createElement('span')
   logLine.innerHTML = `${ms2TimeString(performance.now())} - ${msg}`
@@ -79,7 +76,7 @@ const addToLog = (msg) => {
 
 let heliaInstance = null
 let libp2pInstance = null
-const createHelia = async () => {
+const instantiateHeliaNode = async () => {
 
   // application-specific data lives in the datastore
   const datastore = new DatastoreCore.MemoryDatastore()
@@ -94,84 +91,7 @@ const createHelia = async () => {
     /**
      * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#customizing-libp2p
      */
-    libp2pInstance = await Libp2P.createLibp2p({
-      /**
-       * @see https://github.com/ipfs/js-ipfs-interfaces/tree/master/packages/interface-datastore
-       */
-      datastore,
-      /**
-       * @see https://github.com/libp2p/js-libp2p-kad-dht
-       * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#customizing-dht
-       */
-      dht: Libp2PKadDht.kadDHT({                        // The DHT options (and defaults) can be found in its documentation
-        kBucketSize: 5,
-        clientMode: true,
-        enabled: true,
-        randomWalk: {
-          enabled: true,            // Allows to disable discovery (enabled by default)
-          interval: 300e3,
-          timeout: 10e3
-        }
-      }),
-      /**
-       * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#transport
-       */
-      transports: [
-        Libp2PWebsockets.webSockets(),
-      ],
-      /**
-       * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#connection-encryption
-       */
-      connectionEncryption: [
-        noise()
-      ],
-      /**
-       * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#stream-multiplexing
-       */
-      streamMuxers: [
-        ChainsafeLibp2PYamux.yamux(),
-        Libp2PMplex.mplex(),
-      ],
-      /**
-       * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#customizing-peer-discovery
-       */
-      peerDiscovery: [
-        Libp2PBootstrap.bootstrap({
-          list: [
-            "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
-            "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
-            "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
-            "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt"
-          ],
-        }),
-      ],
-      /**
-       * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#configuring-dialing
-       */
-      dialer: {
-        dialTimeout: 120000,
-      },
-      /**
-       * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#configuring-connection-manager
-       */
-      connectionManager: {
-        // Auto connect to discovered peers (limited by ConnectionManager minConnections)
-        autoDial: true,
-      },
-      /**
-       * @see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md#configuring-peerstore
-       */
-      peerStore: {
-        persistence: true,
-        /**
-         * The threshold number represents the maximum number of "dirty peers" allowed in the PeerStore, i.e. peers that
-         * are not updated in the datastore. In this context, browser nodes should use a threshold of 1, since they
-         * might not "stop" properly in several scenarios and the PeerStore might end up with unflushed records when the
-         * window is closed.
-         */
-        threshold: 1
-      },
-    })
+    libp2pInstance = await createLibp2pInstance({ datastore })
     addToLog('Created LibP2P instance')
 
   }
@@ -190,16 +110,16 @@ window.discoveredPeers = new Map()
 
 const updateConnectedPeers = () => {
   const peers = helia.libp2p.getPeers()
-  connectedPeerCount.innerHTML = peers.length
-  connectedPeersList.innerHTML = ''
+  connectedPeerCountEl.innerHTML = peers.length
+  connectedPeersListEl.innerHTML = ''
   for (const peer of peers) {
     const peerEl = document.createElement('li')
     peerEl.innerText = peer.toString()
-    connectedPeersList.appendChild(peerEl)
+    connectedPeersListEl.appendChild(peerEl)
   }
 }
 
 const updateDiscoveredPeers = () => {
-  discoveredPeerCount.innerHTML = discoveredPeers.size
+  discoveredPeerCountEl.innerHTML = discoveredPeers.size
 }
 
