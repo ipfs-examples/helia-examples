7b1f8f5f9bcc122b72f695a50274fde59bddac62
Taylor Hulsmans <thulsmans.1133@gmail.com>
2023-04-03 19:55:52 -0700

    leave be for now

--- examples/helia-vue/vite.config.js
+++ examples/helia-vue/vite.config.js
@@ -6,9 +6,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  optimizeDeps: {
-    disabled: true
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
