24cd9c33f62075e176087ef656de66dc7fa7111d
Taylor Hulsmans <thulsmans.1133@gmail.com>
2023-04-03 19:50:03 -0700

    try disablel opts

--- examples/helia-vue/vite.config.js
+++ examples/helia-vue/vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   optimizeDeps: {
-    exclude: ['interface-datastore/key']
+    disabled: true
   },
   resolve: {
     alias: {
