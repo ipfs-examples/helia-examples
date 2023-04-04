46f058d6b777df14f0990ce87dd9042d6a845a55
Taylor Hulsmans <thulsmans.1133@gmail.com>
2023-04-03 19:44:01 -0700

    try excludes

--- examples/helia-vue/vite.config.js
+++ examples/helia-vue/vite.config.js
@@ -6,6 +6,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: {
+    exclude: ['interface-datastore/key']
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
