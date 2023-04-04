b39b316b1f418766a8b0939a1567b3cd1fb598cd
Taylor Hulsmans <thulsmans.1133@gmail.com>
2023-04-03 19:26:09 -0700

    try exclude optimizedeps

--- examples/helia-vue/vite.config.js
+++ examples/helia-vue/vite.config.js
@@ -6,10 +6,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      external: ['./node_modules/datasstore-core']
-    }
+  optimizeDeps: {
+    exclude: ['datastore-core']
   },
   resolve: {
     alias: {
