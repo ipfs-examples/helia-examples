8c40a7fa02a1d8c6f32e915730b6761b1bf77569
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-07-27 16:33:39 -0700

    fix(helia-webpack): remove ReactDom.render
    see https://react.dev/blog/2022/03/08/react-18-upgrade-guide\#updates-to-client-rendering-apis

--- examples/helia-webpack/src/index.js
+++ examples/helia-webpack/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './app.css'
 import App from './app.js'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App tab="home"/>)
