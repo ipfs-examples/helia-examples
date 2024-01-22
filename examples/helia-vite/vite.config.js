c900274c2819ae320262009974d9a770e5a72955
Leon <55654280+w-66@users.noreply.github.com>
2024-01-22 18:33:42 +0800

    fix: don't include a leading slash ('/') in path.resolve parameters (#161)
    This breaks running the example on Windows.
    
    Co-authored-by: Alex Potsides <alex@achingbrain.net>

--- examples/helia-vite/vite.config.js
+++ examples/helia-vite/vite.config.js
@@ -5,6 +5,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, '/src') }]
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
   }
 })
