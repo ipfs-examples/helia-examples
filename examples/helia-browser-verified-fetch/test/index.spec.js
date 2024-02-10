32450e45642aa905914d3957fb503835809e6526
Alex Potsides <alex@achingbrain.net>
2024-02-10 21:46:47 +0100

    chore: add test to verified fetch example  (#290)
    * chore: add test to verified fetch example
    
    Adds a test for JSON-codec data
    
    * chore: test verified fetch during ci
    
    * chore: add tests for other types

--- examples/helia-browser-verified-fetch/test/index.spec.js
+++ examples/helia-browser-verified-fetch/test/index.spec.js
@@ -1,27 +1,65 @@
 import { setup, expect } from 'test-ipfs-example/browser'
+import fixtures from './fixtures.js'
 
 // Setup
 const test = setup()
 
-const testCidPath = 'ipfs://bagaaieracglt4ey6qsxtvzqsgwnsw3b6p2tb7nmx5wdgxur2zia7q6nnzh7q'
-
 test.describe('Use @helia/verified-fetch With react and vite', () => {
   // DOM
   const ipfsPathInput = '#ipfs-path'
-  // const fetchOutput = '#output'
-  // const fetchAutoBtn = '#button-fetch-auto'
+  const fetchOutput = '#output'
+  const fetchAutoBtn = '#button-fetch-auto'
 
   test.beforeEach(async ({ servers, page }) => {
     await page.goto(servers[0].url)
   })
 
-  test('should properly render ui with the ipfs path input', async ({ page }) => {
+  test('should properly render ui with the ipfs path input and display JSON', async ({ page }) => {
     // wait for helia node to be online
     const ipfsPath = await page.locator(ipfsPathInput)
     await expect(ipfsPath).toHaveClass(/bg-gray-50/)
 
-    await page.fill(ipfsPathInput, testCidPath)
-    // await page.click(fetchAutoBtn)
-    // await page.locator(fetchOutput).waitFor('visible')
+    await page.fill(ipfsPathInput, `ipfs://${fixtures.json.cid.toString()}`)
+    await page.click(fetchAutoBtn)
+    await page.locator(fetchOutput).waitFor('visible')
+
+    const output = await page.locator(fetchOutput)
+    await expect(output).toContainText(
+      '{ "hello": "world" }',
+      { timeout: 2000 }
+    )
+  })
+
+  test('should properly render ui with the ipfs path input and display DAG-JSON', async ({ page }) => {
+    // wait for helia node to be online
+    const ipfsPath = await page.locator(ipfsPathInput)
+    await expect(ipfsPath).toHaveClass(/bg-gray-50/)
+
+    await page.fill(ipfsPathInput, `ipfs://${fixtures.dagJson.cid.toString()}`)
+    await page.click(fetchAutoBtn)
+    await page.locator(fetchOutput).waitFor('visible')
+
+    const output = await page.locator(fetchOutput)
+    await expect(output).toContainText(
+      '{ "hello": "world" }',
+      { timeout: 2000 }
+    )
+  })
+
+  // TODO: DAG-CBOR is broken - https://github.com/ipfs/helia/pull/426
+  test.skip('should properly render ui with the ipfs path input and display DAG-CBOR', async ({ page }) => {
+    // wait for helia node to be online
+    const ipfsPath = await page.locator(ipfsPathInput)
+    await expect(ipfsPath).toHaveClass(/bg-gray-50/)
+
+    await page.fill(ipfsPathInput, `ipfs://${fixtures.dagCbor.cid.toString()}`)
+    await page.click(fetchAutoBtn)
+    await page.locator(fetchOutput).waitFor('visible')
+
+    const output = await page.locator(fetchOutput)
+    await expect(output).toContainText(
+      '{ "hello": "world" }',
+      { timeout: 2000 }
+    )
   })
 })
