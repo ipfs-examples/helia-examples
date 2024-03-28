84a9e041a89fba39766b561d803c2c2921db3353
Daniel Norman <1992255+2color@users.noreply.github.com>
2024-03-28 13:49:56 +0100

    feat: add helia/verified-fetch browser example (#285)
    * feat: add helia/verified-fetch browser example
    
    * feat(verified-fetch): improve UX (#286)
    
    * Update examples/helia-browser-react-verified-fetch/package.json
    
    Co-authored-by: Alex Potsides <alex@achingbrain.net>
    
    * refactor: clean up code and reduce deps
    
    * chore: rename folder
    
    * chore: add test
    
    * chore: fix linting
    
    * chore: add test to verified fetch example  (#290)
    
    * chore: add test to verified fetch example
    
    Adds a test for JSON-codec data
    
    * chore: test verified fetch during ci
    
    * chore: add tests for other types
    
    * fix: improve rendering of cids on narrow viewports
    
    * fix: improve auto content detection
    
    * chore: fix tests
    
    * chore: upgrade verified-fetch
    
    * feat: add link to the repo
    
    * chore: bump verified fetch version
    
    * fix: add aborting functionality
    
    * fix: handle abort error
    
    * feat: add ability to fetch as dag-json
    
    * chore: update deps
    
    * chore: remove comment
    
    * chore: address feedback and simplify code
    
    * chore: reduce code duplication and add docs
    
    * feat: add links to api docs and source
    
    * fix: linting error
    
    ---------
    
    Co-authored-by: Daniel N <2color@users.noreply.github.com>
    Co-authored-by: Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
    Co-authored-by: Alex Potsides <alex@achingbrain.net>

---
+++ examples/helia-browser-verified-fetch/src/constants.ts
@@ -0,0 +1,26 @@
+export const helpText = `Example Paths
+=========
+IPNS name 👇
+ipns://k51qzi5uqu5dhp48cti0590jyvwgxssrii0zdf19pyfsxwoqomqvfg6bg8qj3s
+
+DNSLink 👇
+ipns://tokens.uniswap.org
+
+JSON 👇
+ipfs://bagaaieracglt4ey6qsxtvzqsgwnsw3b6p2tb7nmx5wdgxur2zia7q6nnzh7q
+
+DAG-CBOR 👇
+ipfs://bafyreicnokmhmrnlp2wjhyk2haep4tqxiptwfrp2rrs7rzq7uk766chqvq
+
+UnixFS JSON 👇
+ipfs://bafybeia5ci747h54m2ybc4rf6yqdtm6nzdisxv57pk66fgubjsnnja6wq4
+
+dag-json 👇
+ipfs://baguqeerasords4njcts6vs7qvdjfcvgnume4hqohf65zsfguprqphs3icwea
+
+UnixFS image 👇
+ipfs://bafybeicklkqcnlvtiscr2hzkubjwnwjinvskffn4xorqeduft3wq7vm5u4
+
+UnixFS video 👇
+ipfs://bafybeicq6y27fphdisjtxaybzxold7dczhvxiiyn3bvkyht7b36lveerrm
+`
