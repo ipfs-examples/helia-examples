402c6c9445937080d4469e98e2a3f0510c6850d7
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-10-24 14:19:30 -0500

    chore: bun test passing now, remove polyfill

--- examples/helia-101/test/index.spec.js
+++ examples/helia-101/test/index.spec.js
@@ -2,26 +2,6 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { waitForOutput } from 'test-ipfs-example/node'
 
-/**
- * @see https://bun.sh/guides/util/detect-bun
- */
-if (process.versions.bun) {
-  // eslint-disable-next-line no-console
-  console.log('Running tests with bun')
-  /**
-   * Polyfill required for bun for now
-   *
-   * @see https://github.com/ipfs-examples/helia-examples/pull/101#issuecomment-2436128883
-   */
-  globalThis.CustomEvent = globalThis.CustomEvent ?? class CustomEventPolyfill extends Event {
-    constructor (message, data) {
-      super(message, data)
-      // @ts-expect-error could be undefined
-      this.detail = data?.detail
-    }
-  }
-}
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 await waitForOutput('Added file contents: Hello World 101', 'node', [path.resolve(__dirname, '../101-basics.js')])
