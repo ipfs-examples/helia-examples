826ca135e352cb6be279268b766855cfe616f900
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-10-24 13:04:05 -0500

    chore: update nextjs example

--- examples/helia-nextjs/next.config.js
+++ examples/helia-nextjs/next.config.js
@@ -1,9 +1,34 @@
-export default {
+/** @type {import('next').NextConfig} */
+const config = {
   reactStrictMode: true,
   images: {
     loader: 'imgix',
     path: 'http://localhost:3000'
   },
-  output: 'export',
-  distDir: 'dist'
+  distDir: 'dist',
+  webpack: (config) => {
+    /**
+     * @see https://github.com/ipfs/helia/issues/553#issuecomment-2158940930
+     */
+    config.externals.push('node-datachannel/polyfill')
+    return config
+  }
+}
+
+export default (phase) => {
+
+  /**
+   * `next start` requires either `undefined` or `'standalone'` output
+   * @type {import('next').NextConfig['output']}
+   */
+  let output = 'standalone'
+  if (process.env.NODE_ENV === 'test') {
+    // test-browser-example requires  `'export'` output
+    output = 'export'
+  }
+
+  return {
+    ...config,
+    output
+  }
 }
