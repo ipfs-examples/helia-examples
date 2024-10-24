1be6c4759c6718674fc2811a9448ef22aae1f74c
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-10-24 14:04:29 -0500

    chore: simple polyfill for now

--- examples/helia-101/test/index.spec.js
+++ examples/helia-101/test/index.spec.js
@@ -2,6 +2,24 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import { waitForOutput } from 'test-ipfs-example/node'
 
+/**
+ * @see https://bun.sh/guides/util/detect-bun
+ */
+if (process.versions.bun) {
+  /**
+   * Polyfill required for bun for now
+   *
+   * @see https://github.com/ipfs-examples/helia-examples/pull/101#issuecomment-2436128883
+   */
+  globalThis.CustomEvent = globalThis.CustomEvent ?? class CustomEventPolyfill extends Event {
+    constructor (message, data) {
+      super(message, data)
+      // @ts-expect-error could be undefined
+      this.detail = data?.detail
+    }
+  }
+}
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 await waitForOutput('Added file contents: Hello World 101', 'node', [path.resolve(__dirname, '../101-basics.js')])
