190987cce4f4e41d28bcafecb6989470914621fd
Daniel N <2color@users.noreply.github.com>
2025-04-29 15:14:08 +0200

    fix: add try catch

--- examples/helia-101/501-ipns.js
+++ examples/helia-101/501-ipns.js
@@ -16,21 +16,28 @@ const ipns = IPNS(helia)
 const privateKey = await generateKeyPair('Ed25519')
 
 
+console.log(`Created IPNS name:
+  ${privateKey.publicKey.toCID()} (as CID)
+  ${privateKey.publicKey.toString()} (as base58btc string)`)
+
+
 // Use dag-cbor to encode a 
 const cid = await dcbor.add({
   message: 'IPNS can be used to publish mutable pointers to immutable data',
   repo: 'https://github.com/ipfs-examples/helia-examples'
 })
 
-const record = await ipns.publish(privateKey, cid, {
-  lifetime: 1000 * 60 * 60 * 24 * 30, // 30 days
-  ttl: 1000 * 60, // 1 minute TTL
-  signal: AbortSignal.timeout(60_000), // 60 seconds timeout for publishing
-})
+try {
+  const record = await ipns.publish(privateKey, cid, {
+    lifetime: 1000 * 60 * 60 * 24 * 30, // 30 days
+    ttl: 1000 * 60, // 1 minute TTL
+    signal: AbortSignal.timeout(60_000), // 60 seconds timeout for publishing
+  })
+  console.log('Published IPNS record: ', record)
 
-console.log(`Published IPNS name: 
-  ${privateKey.publicKey.toCID()} (as CID) 
-  ${privateKey.publicKey.toString()} (as base58btc string)`)
-console.log('Published IPNS record: ', record)
+} catch (error) {
+  console.error('Error publishing IPNS record:', error)
+}
+
 
 await helia.stop()
